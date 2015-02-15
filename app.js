4880bb688068c49d3bd792e390a5c85e89c3484b
Bruno Macherel <bruno.macherel_AT_gm41l.com>
2015-02-15 12:38:38 +0100

    Add make route cleaner and add API skul to control videos

--- app.js
+++ app.js
@@ -1,35 +1,43 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var app    = require('express')();
+var http   = require('http').Server(app);
+var io     = require('socket.io')(http);
 
+// Handlers
+app.all('*', function(req, res, next) {
+//	console.log(req);
+	next();
+});
+// API
+app.get('/api/:action/*', function(req, res){
+	io.emit(req.params.action, req.params[0]);
+	res.send();
+});
+// Public pages
+var wwwOptions = { root: __dirname + '/www/' };
 app.get('/', function(req, res){
-  res.sendfile('index.html');
+	res.sendFile('index.html', wwwOptions);
 });
-
-app.get('/video.avi', function(req, res){
-  res.sendfile('video.avi');
+app.get('/main.js', function(req, res){
+	res.sendFile('main.js', wwwOptions);
 });
-
-app.get('/video.m4v', function(req, res){
-  res.sendfile('video.m4v');
-});
-
-app.get('/devstories.webm', function(req, res){
-  res.sendfile('devstories.webm');
+// Resources
+var options = { root: __dirname + '/data/' };
+app.get('/:file', function(req, res){
+	res.sendFile(req.params.file, options);
 });
 
 http.listen(3000, function(){
-  console.log('listening on *:3000');
+	console.log('listening on *:3000');
 });
 
 io.on('connection', function(socket){
-  console.log('webpage connected');
+	console.log('webpage connected');
 
-  socket.on('ready', function(msg){
-    console.log('webpage ready');
-  });
+	socket.on('ready', function(msg){
+		console.log('webpage ready');
+	});
 
-  socket.on('disconnect', function(){
-    console.log('webpage disconnected');
-  });
+	socket.on('disconnect', function(){
+		console.log('webpage disconnected');
+	});
 });
